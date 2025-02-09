6ac3735d74166ee3487a6b14231251c3e874ee16
bear3 <114121926+bear3232@users.noreply.github.com>
2025-02-09 09:27:54 +0900

    modify
    modify

--- InitGui.py
+++ InitGui.py
@@ -1,14 +1,60 @@
 import FreeCADGui
 
-
 class RadiossWorkbench(Workbench):
     def __init__(self):
         self.__class__.MenuText = "Radioss"
         self.__class__.ToolTip = "これはカスタムワークベンチの例です"
+        
+        # ツールバーのアイコンリストを定義
+        self.__class__.Icon = """
+            /* XPM */
+            static char * radioss_xpm[] = {
+            "16 16 3 1",
+            " 	c None",
+            ".	c #000000",
+            "+	c #FFFFFF",
+            "                ",
+            "       ..       ",
+            "      .++.      ",
+            "     .++++.     ",
+            "    .++++++.    ",
+            "   .++++++++.   ",
+            "  .++++++++++.  ",
+            " .++++++++++++. ",
+            " .++++++++++++. ",
+            "  .++++++++++.  ",
+            "   .++++++++.   ",
+            "    .++++++.    ",
+            "     .++++.     ",
+            "      .++.      ",
+            "       ..       ",
+            "                "};
+            """
 
     def Initialize(self):
         import RadiossCommands
-        # Add commands to FreeCAD
+
+        # コマンドリストの定義
+        self.analysis_commands = [
+            'Radioss_Analysis',
+            'Radioss_AnalysisProperties'
+        ]
+        
+        self.modeling_commands = [
+            'Radioss_Material',
+            'Radioss_Constraint',
+            'Radioss_Load',
+            'Radioss_Set',
+            'Radioss_RigidBody',
+            'Radioss_Contact'
+        ]
+        
+        self.io_commands = [
+            'Radioss_Import',
+            'Radioss_Export'
+        ]
+
+        # コマンドの追加
         FreeCADGui.addCommand('Radioss_Import', RadiossCommands.RadiossImport())
         FreeCADGui.addCommand('Radioss_Analysis', RadiossCommands.RadiossAnalysis())
         FreeCADGui.addCommand('Radioss_Material', RadiossCommands.RadiossMaterial())
@@ -19,6 +65,16 @@ class RadiossWorkbench(Workbench):
         FreeCADGui.addCommand('Radioss_Contact', RadiossCommands.RadiossContact())
         FreeCADGui.addCommand('Radioss_Export', RadiossCommands.RadiossExport())
         FreeCADGui.addCommand('Radioss_AnalysisProperties', RadiossCommands.RadiossAnalysisProperties())
+
+        # ツールバーの作成
+        self.appendToolbar('Radioss Analysis', self.analysis_commands)
+        self.appendToolbar('Radioss Modeling', self.modeling_commands)
+        self.appendToolbar('Radioss I/O', self.io_commands)
+        
+        # メニューの作成
+        self.appendMenu('Radioss Analysis', self.analysis_commands)
+        self.appendMenu('Radioss Modeling', self.modeling_commands)
+        self.appendMenu('Radioss I/O', self.io_commands)
 
     def Activated(self):
         return
